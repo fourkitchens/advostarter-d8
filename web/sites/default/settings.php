30d1208098113b69a1de29b8e5fdc6aab05b35e2
Dave Hansen-Lange <dalin@18981.no-reply.drupal.org>
2018-08-03 23:56:52 -0400

    More better local config.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -37,6 +37,11 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 if ($config['server_environment'] == 'local') {
   $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
   $settings['skip_permissions_hardening'] = TRUE;
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+  // Front-end devs will want to include the following line in a settings.local.php
+  // Back-end devs should _not_ disable render caching because it will hide
+  // bugs with cache contexts/tags.
+  # $settings['cache']['bins']['render'] = 'cache.backend.null';
 }
 
 if ($config['server_environment'] != 'local') {
