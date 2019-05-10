8cfd0b5c84ab33e3277583c3798611e1351c17c6
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2019-05-10 16:02:06 -0400

    Fixed PHP errors that can cause issues creating a multidev.

--- web/private/drush_search_api_reindex.php
+++ web/private/drush_search_api_reindex.php
@@ -5,11 +5,11 @@ if (function_exists('newrelic_ignore_transaction')) {
 }
 
 echo "Marking all content for reindexing...\n";
-echo "Note: this will only work in a multi-dev if the following patch is applied:\n"
-echo "https://github.com/pantheon-systems/search_api_pantheon/pull/53\n"
+echo "Note: this will only work in a multi-dev if the following patch is applied:\n";
+echo "https://github.com/pantheon-systems/search_api_pantheon/pull/53\n";
 passthru('drush search-api-reindex -y');
 echo "All content marked for reindexing.\n";
 
 echo "Indexing content...\n";
 passthru('drush search-api-index -y');
-echo "All content indexed.\n");
+echo "All content indexed.\n";
